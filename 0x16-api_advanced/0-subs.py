5221411b5b8a9eae26867877d0a6eba8a05f42ce
Your Name <zinebbaq@gmail.com>
2024-08-15 17:37:00 -0700

    version2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,22 @@
-#!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "MyRedditClient/0.1 by yourusername"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+
+    # Check if the request was successful
+    if response.status_code != 200:
         return 0
+
+    try:
+        results = response.json().get("data")
+        if results:
+            return results.get("subscribers", 0)
+        else:
+            return 0
+    except requests.exceptions.JSONDecodeError:
+        return 0
+
